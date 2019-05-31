28a3b4df468e22732c6bf42cd0b12833944c3235
berlioz458 <berlioz458@gmail.com>
2019-05-31 21:05:46 +0700

    добила урок 16

--- src/TestClass16.java
+++ src/TestClass16.java
@@ -23,7 +23,7 @@ public class TestClass16 {
         System.out.println("End of program");
 
         System.out.println();
-        i = 10;
+        i = 90;
 
         if (i > 50){
             System.out.println("i > 50");
@@ -33,7 +33,25 @@ public class TestClass16 {
                 System.out.println("i <= 90");
             }
         }else {
-            System.out.println("i < 50");
+            System.out.println("i <= 50");
         }
+
+
+        System.out.println();
+        i = 10;
+        switch (i){ //аналог IF, действия срабатывают при условии
+            case 1:
+                System.out.println("That number is 1");
+                break;
+            case 2:
+                System.out.println("That number is 2");
+                break;
+            case 3:
+                System.out.println("That number is 3");
+                break;
+            default: //типа else необязательный оператор
+                System.out.println("I don't know the number");
+        }
+
     }
 }
