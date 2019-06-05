67b63de5e6635016d3305ceb0ae20ef95d5f14f9
shulinina.e <test-2@prodv.net>
2019-06-05 12:33:27 +0700

    26 урок про конструкторы класса, внесены изменения в тестовый класс Car из урока 25. Так же добавлены примеры вызова в уже существующий TestClass25.java

--- src/TestClass25.java
+++ src/TestClass25.java
@@ -1,6 +1,6 @@
 public class TestClass25 {
     public static void main(String[] args) {
-        //25. классы и объекты
+        //25. классы и объекты  + 26. конструктор класса
         //класс - описание будущих объектов
         //на основе класса создаются экземляры класса - объекты, которые обладают всеми свойствами класса
         Car car1 = new Car(); // создали объект нашего класса
@@ -21,5 +21,11 @@ public class TestClass25 {
         car3.color = "Red";
         car2.drive(100);
         car3.drive(150);
+
+        Car car4 = new Car("Yellow"); // перегрузили метод конструктор и теперь создаем сразу с цветом
+        System.out.println("Car color: " + car4.color);
+        System.out.println();
+        Car car5 = new Car("Red", 2100,2000,5100);
+        System.out.println(car5.color + " " + car5.height + " " + car5.width + " " + car5.lenght);
     }
 }
